d0f943fd68cc0aba7da5ce707dd86655894528f1
Fritz <fritz@icloud.com>
2022-02-12 19:37:11 -0600

    constructing controller

--- RPN Calculator/Calculator/Calculator/ViewController.swift
+++ RPN Calculator/Calculator/Calculator/ViewController.swift
@@ -8,14 +8,45 @@
 import UIKit
 import RPN
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: - Outlets
+    @IBOutlet weak var textField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textField.delegate = self
 
     }
-
-
+    
+    //MARK: - Actions
+    
+    @IBAction func numberButtonTap(_ sender: UIButton) {
+    }
+    
+    @IBAction func decimalButtonTap(_ sender: UIButton) {
+    }
+    
+    @IBAction func divideButtonTap(_ sender: UIButton) {
+    }
+    
+    @IBAction func multiplyButtonTap(_ sender: UIButton) {
+    }
+    
+    @IBAction func subtractButtonTap(_ sender: UIButton) {
+    }
+    
+    @IBAction func addButtonTap(_ sender: UIButton) {
+    }
+    
+    @IBAction func returnButtonTap(_ sender: UIButton) {
+    }
+    
+    //MARK: - Methods
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        return true
+    }
 }
 
