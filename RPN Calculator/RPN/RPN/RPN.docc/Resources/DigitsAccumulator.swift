f07258b9835bf40ab522c358b62e5c4b298aec42
Fritz <fritz@icloud.com>
2022-02-12 19:17:35 -0600

    completed framework

--- RPN Calculator/RPN/RPN/RPN.docc/Resources/DigitsAccumulator.swift
+++ RPN Calculator/RPN/RPN/RPN.docc/Resources/DigitsAccumulator.swift
@@ -11,6 +11,8 @@ public struct DigitAccumulator {
     
     public init() {}
     
+    //MARK: - Properties
+    
     public enum Digit: Equatable {
         case decimalPoint
         case number(Int)
@@ -19,7 +21,38 @@ public struct DigitAccumulator {
     public var digits = [Digit]()
     
     public enum DigitAccumulatorError: Error {
-    case extraDeciamlPoint
-    case invalidDigitNumberValue
+        case extraDeciamlPoint
+        case invalidDigitNumberValue
     }
+    
+    //MARK: - Methods
+    
+    public func add(digit: Digit) throws {
+        switch digit {
+        case .decimalPoint:
+            if digits.contains(.decimalPoint){
+                throw DigitAccumulatorError.extraDeciamlPoint
+            }
+        case .number(let value):
+            if value < 0 || 9 < value {
+                throw DigitAccumulatorError.invalidDigitNumberValue
+            }
+        }
+        digits.append(digit)
+    }
+    
+    public mutating func clear(){
+        digits.removeAll()
+    }
+    
+    func result() -> Double? {
+        let string = digits.map { (digit) -> String in
+            switch digit {
+            case .decimalPoint:
+                return "."
+            case .number(let value):
+                return String(value)
+        }
+        }.joined()
+        return Double(string)
 }
